a1e5aad85bcea0a59387c7786511174af8ec32bb
Ashish Mishra <asismisr@Administrators-MacBook-Pro.local>
2023-12-16 12:25:40 +0530

    Adding arraylist code

---
+++ src/main/java/collectionsPrograms/arrayListCode/allMethods/arrayListAllMethods.java
@@ -0,0 +1,34 @@
+/** add, addAll, remove, heterogeneous elements
+ *
+ */
+package collectionsPrograms.arrayListCode.allMethods;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class arrayListAllMethods {
+
+    public static void main(String[] args){
+
+        // heterogeneous elements storing
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add('A');
+        arrayList.add(2);
+        arrayList.add(null);
+        arrayList.add(12.5);
+        arrayList.add(null);
+        arrayList.add("B");
+        System.out.println("arrayList elements"+arrayList);
+
+        // removing elements
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        arrayList2.add("A");
+        arrayList2.add("B");
+        arrayList2.add("C");
+        arrayList2.add("D");
+        arrayList2.remove(2);
+        arrayList2.remove("D");
+        System.out.println("arrayList2 elements"+arrayList2);
+    }
+}
