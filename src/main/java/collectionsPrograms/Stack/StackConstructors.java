74488557a68361f41c0bc281ecf368c3726cc0d7
Ashish Mishra <asismisr@Administrators-MacBook-Pro.local>
2023-12-17 17:31:09 +0530

    Adding stack programs

---
+++ src/main/java/collectionsPrograms/Stack/StackConstructors.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+package collectionsPrograms.Stack;
+
+import java.util.Stack;
+
+public class StackConstructors {
+
+    public static void main(String[] args){
+        Stack<String> stack = new Stack<>();
+        System.out.println("stack elements:"+stack);
+        System.out.println("stack Size:"+stack.size());
+    }
+}
---
+++ src/main/java/collectionsPrograms/Stack/StackMethods.java
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+package collectionsPrograms.Stack;
+
+import java.util.Stack;
+
+public class StackMethods {
+
+    public static void main(String[] args){
+
+        Stack<String> stack1 = new Stack<>();
+        stack1.push("A");
+        stack1.push("B");
+        stack1.push("C");
+        stack1.push("D");
+        stack1.push("E");
+        System.out.println("stack1 Elements:"+stack1);
+
+        System.out.println("element pop:"+stack1.pop());
+        System.out.println("stack1 Elements:"+stack1);
+
+        System.out.println("stack1 Peek:"+stack1.peek());
+        System.out.println("stack1 Size:"+stack1.size());
+
+        System.out.println("stack1 Elements:"+stack1);
+        System.out.println("stack1 Search:"+stack1.search("B"));
+        System.out.println("stack1 Search:"+stack1.search('B'));
+
+    }
+}
